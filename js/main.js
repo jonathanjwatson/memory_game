ddda29b6428371ac71e9b9fa5d92de52b31d45e7
jwats287 <jwats287@users.noreply.github.com>
2017-06-18 14:13:43 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,9 +1,27 @@
 console.log("Up and running!");
 
-var cardOne = "Queen";
-var cardTwo = "Queen";
-var cardThree = "King";
-var cardFour = "King";
+var cards = ["queen", "queen", "king", "king"];
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+
+cardsInPlay.push(cardOne);
+
+console.log("User flipped" + " " + cardOne);
+
+var cardTwo = cards[2];
+
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped" + " " + cardTwo);
+
+if (cardsInPlay.length === 2){
+	if(cardsInPlay[0] === cardsInPlay[1]){
+		alert("You found a match!");
+	}
+	else{
+		alert("Sorry! Try again.");
+	};
+}
+else{};

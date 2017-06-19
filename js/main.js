0c71daf02f57c7b7eef00fb4abe7dc8fe7e77892
jwats287 <jwats287@users.noreply.github.com>
2017-06-18 20:55:21 -0400

    changed from array to object

--- js/main.js
+++ js/main.js
@@ -1,6 +1,27 @@
 console.log("Up and running!");
 
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+   {
+       rank: 'queen',
+       suit: 'hearts',
+       cardImage: 'images/queen-of-hearts.png'
+   },
+   {
+       rank: 'queen',
+       suit: 'diamonds',
+       cardImage: 'images/queen-of-diamonds.png'
+   },
+   {
+       rank: 'king',
+       suit: 'hearts',
+       cardImage: 'images/king-of-hearts.png'
+   },
+   {
+       rank: 'king',
+       suit: 'diamonds',
+       cardImage: 'images/king-of-diamonds.png'
+   }
+];
 
 var cardsInPlay = [];
 
@@ -18,8 +39,10 @@ function checkForMatch(){
 
 
 function flipCard(cardId){
-	console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+	console.log(cards[cardId].suit);
+	console.log(cards[cardId].cardImage);
+	cardsInPlay.push(cards[cardId].rank);
 	checkForMatch();
 };
 
